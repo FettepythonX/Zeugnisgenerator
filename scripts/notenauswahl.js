4559b56d508b3098fb755bedc17c6b57259de67f
Anna-Lou-srh <anna-lou.reiner@web.de>
2022-11-17 10:25:04 +0100

    mein CSS Design Vorschlag

--- scripts/notenauswahl.js
+++ scripts/notenauswahl.js
@@ -11,8 +11,8 @@ let ausbilder;
 let ausbilderName;
 let praktikum;
 let programme = "";
-let tabColorWrong = "rgb(255,0,0)";
-let tabColorRight = "rgb(15, 148, 53)";
+let tabColorWrong = "lightcoral";
+let tabColorRight = "lightgreen";
 let categories = ["Fachwissen", "Einsatz", "Arbeitsweise", "Pensum", "Arbeitserfolg", "Leistung", "Soziales", "Abschied"];
 
 
